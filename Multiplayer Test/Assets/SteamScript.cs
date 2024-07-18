85a11b227f94b40637d462a27d06331ff2e6a4fe
JohanAbrahamssonHis <118519456+JohanAbrahamssonHis@users.noreply.github.com>
2024-07-18 22:32:05 +0200

    Basic Steam Support

---
+++ Multiplayer Test/Assets/SteamScript.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+using Steamworks;
+
+public class SteamScript : MonoBehaviour {
+    void Start() {
+        if(SteamManager.Initialized) {
+            string name = SteamFriends.GetPersonaName();
+            Debug.Log(name);
+        }
+    }
+}
